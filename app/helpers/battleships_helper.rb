4223d37ddcc6f5b405abc5a5a258e7c288f7fedf
Daniel Hathorn <dmhathorn@gmail.com>
2011-04-16 00:40:10 -0600

    added js more link

--- app/helpers/battleships_helper.rb
+++ app/helpers/battleships_helper.rb
@@ -1,5 +1,19 @@
 module BattleshipsHelper
+
+  def truncate_and_link(battleship)
+    trunc = truncate(battleship.description, :length => 55)
+    trunc +="#{show_more(battleship.id)}" if battleship.description.length > 55
+    trunc
+  end
   
+  def show_more(battleship)
+    link_to_function("more") do |page|
+      page.visual_effect :blind_down, "notrunc#{battleship}"
+      page.toggle "trunc#{battleship}"
+      page.toggle "notrunc#{battleship}"
+    end
+  end
+
   def owned_by(current_user, battleship) 
    battleship.user == current_user
   end
